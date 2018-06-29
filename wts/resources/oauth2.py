21bc188260bac63ab71376e9f2d4c8d46200a1ff
philloooo <phillis.tt@gmail.com>
2018-06-29 00:02:12 -0500

    fix(auth): get identity from id_token

--- wts/resources/oauth2.py
+++ wts/resources/oauth2.py
@@ -19,17 +19,17 @@ def client_do_authorize():
             'could not authorize; state did not match across auth requests'
         )
     try:
-        token = flask.current_app.oauth2_client.fetch_access_token(
+        tokens = flask.current_app.oauth2_client.fetch_access_token(
             redirect_uri, **flask.request.args.to_dict()
         )
-        return refresh_refresh_token(token['refresh_token'])
+        return refresh_refresh_token(tokens)
     except KeyError as e:
-        raise AuthError('error in token response: {}'.format(token))
+        raise AuthError('error in token response: {}'.format(tokens))
     except (OAuth2Error, OAuthException) as e:
         raise AuthError(str(e))
 
 
-def refresh_refresh_token(token):
+def refresh_refresh_token(tokens):
     options = {
         'verify_signature': False,
         'verify_aud': False,
@@ -42,19 +42,21 @@ def refresh_refresh_token(token):
         'verify_at_hash': False,
         'leeway': 0
     }
-    content = jwt.decode(token, key=None, options=options)
+    refresh_token = tokens['refresh_token']
+    id_token = tokens['id_token']
+    content = jwt.decode(id_token, key=None, options=options)
     userid = content['sub']
     for old_token in db.session.query(RefreshToken).filter_by(userid=userid):
         db.session.delete(old_token)
     if hasattr(flask.current_app, 'encryption_key'):
-        token = flask.current_app.encryption_key.encrypt(
-            bytes(token, encoding='utf8')
+        refresh_token = flask.current_app.encryption_key.encrypt(
+            bytes(refresh_token, encoding='utf8')
         )
 
     new_token = RefreshToken(
-        token=token, userid=userid,
+        token=refresh_token, userid=userid,
         username=content['context']['user']['name'],
         jti=content['jti'], expires=content['exp'])
     db.session.add(new_token)
     db.session.commit()
-    return token
+    return refresh_token
