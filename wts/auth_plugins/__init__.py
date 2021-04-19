5c46af1bc9afbcf44a4334ef65771b1921027949
John McCann <johnmccann@uchicago.edu>
2021-04-19 12:48:43 -0700

    feat(/token): optimize which auth plugins run

--- wts/auth_plugins/__init__.py
+++ wts/auth_plugins/__init__.py
@@ -3,17 +3,9 @@ from .k8s import K8SPlugin
 import flask
 
 
-def setup_plugins(app):
-    app.auth_plugins = []
-    for plugin in app.config["AUTH_PLUGINS"]:
-        if plugin == "default":
-            app.auth_plugins.append(DefaultPlugin())
-        elif plugin == "k8s":
-            app.auth_plugins.append(K8SPlugin())
-
-
 def find_user():
-    for plugin in flask.current_app.auth_plugins:
-        user = plugin.find_user()
+    if flask.request.headers.get("Authorization"):
+        user = DefaultPlugin().find_user()
         if user:
             return user
+    return K8SPlugin().find_user()
