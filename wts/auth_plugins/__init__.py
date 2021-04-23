a901ce9cb094dd7aa89ab6e8de8328e36ab29a63
John McCann <johnmccann@uchicago.edu>
2021-04-23 09:05:55 -0700

    feat(/token): don't print auth header in find_user

--- wts/auth_plugins/__init__.py
+++ wts/auth_plugins/__init__.py
@@ -4,9 +4,6 @@ import flask
 
 
 def find_user():
-    auth_header = flask.request.headers.get("Authorization")
-    print("auth_header: " + str(auth_header))
-
-    if auth_header:
+    if flask.request.headers.get("Authorization"):
         return DefaultPlugin().find_user()
     return K8SPlugin().find_user()
