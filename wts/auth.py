c7b564648f41e5492d8a5c1d8ca25e1c5961f82b
John McCann <johnmccann@uchicago.edu>
2021-07-15 12:51:30 -0700

    Merge pull request #35 from uc-cdis/fix/do-not-allow-infinite-access-tokens
    (PXP-8420): Do not allow infinite access tokens

--- wts/auth.py
+++ wts/auth.py
@@ -1,16 +1,10 @@
-from functools import wraps
 import flask
+
 from .auth_plugins import find_user
 from cdiserrors import AuthError
 
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not hasattr(flask.g, "user"):
-            flask.g.user = find_user()
-            if not flask.g.user:
-                raise AuthError("You need to be authenticated to use this resource")
-        return f(*args, **kwargs)
-
-    return decorated_function
+def authenticate(allow_access_token=False):
+    flask.g.user = find_user(allow_access_token)
+    if not flask.g.user:
+        raise AuthError("You need to be authenticated to use this resource")
--- wts/auth_plugins/__init__.py
+++ wts/auth_plugins/__init__.py
@@ -1,9 +1,9 @@
-from .base import DefaultPlugin
+from .base import AccessTokenPlugin
 from .k8s import K8SPlugin
 import flask
 
 
-def find_user():
-    if flask.request.headers.get("Authorization"):
-        return DefaultPlugin().find_user()
+def find_user(allow_access_token=False):
+    if allow_access_token and flask.request.headers.get("Authorization"):
+        return AccessTokenPlugin().find_user()
     return K8SPlugin().find_user()
