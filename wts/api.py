6bd9660dc9c4d207f8fb5a10210810e0a3b3cdf4
John McCann <johnmccann@uchicago.edu>
2022-08-09 16:43:15 -0500

    chore(agg allowlist): make fully customizable

--- wts/api.py
+++ wts/api.py
@@ -97,8 +97,8 @@ def load_settings(app):
     app.config["COMMONS_HOSTNAMES"] = list(
         set(config["commons_hostname"] for config in app.config["OIDC"].values())
     )
-    app.config["AGGREGATE_ENDPOINT_ALLOWLIST"] = set(
-        ["/user/user", "/authz/mapping"] + get_var("AGGREGATE_ENDPOINT_ALLOWLIST", [])
+    app.config["AGGREGATE_ENDPOINT_ALLOWLIST"] = get_var(
+        "AGGREGATE_ENDPOINT_ALLOWLIST", []
     )
     app.config["SESSION_COOKIE_NAME"] = "wts"
     app.config["SESSION_COOKIE_SECURE"] = True
