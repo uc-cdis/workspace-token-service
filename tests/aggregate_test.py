1741a0b25fcef447042487c016e52f58bec89b8f
John McCann <johnmccann@uchicago.edu>
2022-08-10 08:15:49 -0500

    docs(aggregate_test.py): add docstrings

--- tests/aggregate_test.py
+++ tests/aggregate_test.py
@@ -10,6 +10,11 @@ from .conftest import (
 def test_aggregate_user_user_endpoint(
     app, client, persisted_refresh_tokens, auth_header
 ):
+    """
+    Test aggregate endpoint using `/user/user`. Expect a
+    200 response with aggregate data returned from
+    the default commons and "idp_a".
+    """
     res = client.get("/aggregate/user/user", headers=auth_header)
     assert res.status_code == 200
     assert len(res.json) == 2
@@ -32,6 +37,9 @@ def test_aggregate_user_user_endpoint(
 def test_aggregate_user_user_endpoint_with_filters(
     app, client, persisted_refresh_tokens, auth_header
 ):
+    """
+    Test that aggregate endpoint successfully returns only specified filters.
+    """
     res = client.get(
         "/aggregate/user/user?filters=authz&filters=role", headers=auth_header
     )
@@ -42,6 +50,7 @@ def test_aggregate_user_user_endpoint_with_filters(
     assert default_commons_hostname in res.json
     assert len(res.json[default_commons_hostname]) == 2
     assert "role" in res.json[default_commons_hostname]
+    assert "authz" in res.json[default_commons_hostname]
     assert_authz_mapping_for_test_user_in_default_commons(
         res.json[default_commons_hostname]["authz"]
     )
@@ -56,22 +65,31 @@ def test_aggregate_user_user_endpoint_with_filters(
     )
 
 
-def test_aggregate_user_user_endpoint_with_wrong_filter(
+def test_aggregate_user_user_endpoint_with_absent_filter(
     app, client, persisted_refresh_tokens, auth_header
 ):
-    res = client.get("/aggregate/user/user?filters=wrong", headers=auth_header)
+    """
+    Test that aggregate endpoint returns a successful response when filters
+    specified are absent. Check that absence is indicated via a special
+    value (i.e. JSON `null`).
+    """
+    res = client.get("/aggregate/user/user?filters=absent", headers=auth_header)
     assert res.status_code == 200
 
     default_commons_hostname = app.config["OIDC"]["default"]["commons_hostname"]
-    assert res.json[default_commons_hostname]["wrong"] is None
+    assert res.json[default_commons_hostname]["absent"] is None
 
     idp_a_commons_hostname = app.config["OIDC"]["idp_a"]["commons_hostname"]
-    assert res.json[idp_a_commons_hostname]["wrong"] is None
+    assert res.json[idp_a_commons_hostname]["absent"] is None
 
 
 def test_aggregate_endpoint_when_one_linked_commons_returns_500(
     app, client, persisted_refresh_tokens, auth_header, respx_mock
 ):
+    """
+    Test aggregate endpoint when one linked commons returns a 500.
+    Expect that WTS still returns a 200.
+    """
     idp_a_fence_url = app.config["OIDC"]["idp_a"]["api_base_url"].rstrip("/")
     respx_mock.get(f"{idp_a_fence_url}/user").mock(return_value=httpx.Response(500))
     res = client.get("/aggregate/user/user", headers=auth_header)
@@ -92,6 +110,10 @@ def test_aggregate_endpoint_when_one_linked_commons_returns_500(
 def test_aggregate_endpoint_with_anonymous_request(
     app, client, persisted_refresh_tokens, respx_mock
 ):
+    """
+    Test aggregate endpoint when making an anonymous request. Expect
+    a 200 response containing data from all configured commons.
+    """
     commons_hostnames = app.config["COMMONS_HOSTNAMES"]
     for commons_hostname in commons_hostnames:
         respx_mock.get(f"https://{commons_hostname}/index/index").mock(
@@ -111,5 +133,9 @@ def test_aggregate_endpoint_with_anonymous_request(
 
 
 def test_aggregate_with_endpoint_not_in_allowlist(client, auth_header):
+    """
+    Test that aggregate endpoint returns a 404 when the requested endpoint is
+    not in configured allowlist.
+    """
     res = client.get("/aggregate/user/credentials/api", headers=auth_header)
     assert res.status_code == 404
